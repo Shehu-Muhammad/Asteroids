05d15900053541469fb2e59339c16ef2aeec87a2
Shehu Muhammad <shehuaamuhammad@gmail.com>
2026-08-03 14:41:07 -0400

    logged game state

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    print(f'Starting asteroids!')
+    print(f'Starting Asteroids')
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
